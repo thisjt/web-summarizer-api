eac9f32ee4ea7cfd395db93df2e12d23f549ec64
Joshua Tahanlangit <contact@thisjt.me>
2024-10-11 02:41:19 +0800

    feat: post job queue

--- src/routes/job/postJob.js
+++ src/routes/job/postJob.js
@@ -1,7 +1,44 @@
+import emit from '$lib/emit';
 import { app } from '$lib/express';
+import { StatusCodes } from 'http-status-codes';
+import { z } from 'zod';
+import prisma from '$lib/prisma';
+
+const jobPost = z.object({
+	url: z.string({ message: 'URL is missing. Expecting { url : "http(s)://..." }.' }).url({ message: 'URL is invalid.' }),
+});
 
 export default function postJob() {
-	app.post('/job', (request, response) => {
-		response.json({ job: 0 });
+	app.post('/job', async (request, response) => {
+		const parseResult = jobPost.safeParse(request.body);
+
+		if (parseResult.error) {
+			response.status(StatusCodes.BAD_REQUEST).json(emit({ errors: parseResult }));
+			return;
+		}
+
+		let result;
+		try {
+			result = await prisma.jobs.create({
+				data: {
+					url: parseResult.data.url,
+					status: 'queue',
+					timestamp: new Date().getTime(),
+				},
+				select: {
+					id: true,
+				},
+			});
+		} catch (e) {
+			response.status(StatusCodes.INTERNAL_SERVER_ERROR).json(emit({ errors: { db: ['Database error.'] }, debug: e }));
+			return;
+		}
+
+		if (!result.id) {
+			response.status(StatusCodes.SERVICE_UNAVAILABLE).json(emit({ errors: { db: ['Unable to create job.'] } }));
+			return;
+		}
+
+		response.json(emit({ id: result.id, url: parseResult.data.url, status: 'queue' }));
 	});
 }
