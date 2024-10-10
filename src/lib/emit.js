ba52efa8a9dc1413777961011ea99a72bdc0d3e9
Joshua Tahanlangit <contact@thisjt.me>
2024-10-11 03:04:39 +0800

    refactor: differentiate zod errors from regular errors

--- src/lib/emit.js
+++ src/lib/emit.js
@@ -4,7 +4,8 @@ const LOGLEVEL = 'debug';
  * @typedef {import('zod').SafeParseError<any>} SafeParseError
  *
  * @param {object} data
- * @param {SafeParseError | Object<string, string[]>} [data.errors]
+ * @param {Object<string, string[]>} [data.errors]
+ * @param {SafeParseError} [data.zodErrors]
  * @param {number} [data.id]
  * @param {string} [data.url]
  * @param {"queue" | "processing" | "completed" | "failed" | string} [data.status]
@@ -14,10 +15,10 @@ export default function emit(data) {
 	if (LOGLEVEL !== 'debug') delete data.debug;
 
 	if (data.errors) {
-		if (!Array.isArray(data.errors.error)) {
-			return { success: false, errors: data.errors.error.flatten().fieldErrors };
-		} else {
-			return { success: false, errors: data.errors };
-		}
-	} else return { ...data, success: true };
+		return { success: false, errors: data.errors };
+	} else if (data.zodErrors) {
+		return { success: false, errors: data.zodErrors.error.flatten().fieldErrors };
+	} else {
+		return { ...data, success: true };
+	}
 }
