1905054b93cf7699d93c63f718dfd8a63defbb75
Joshua Tahanlangit <contact@thisjt.me>
2024-10-11 02:40:05 +0800

    feat: response json output handler

---
+++ src/lib/emit.js
@@ -0,0 +1,23 @@
+const LOGLEVEL = 'debug';
+
+/**
+ * @typedef {import('zod').SafeParseError<any>} SafeParseError
+ *
+ * @param {object} data
+ * @param {SafeParseError | Object<string, string[]>} [data.errors]
+ * @param {number} [data.id]
+ * @param {string} [data.url]
+ * @param {"queue" | "processing" | "completed" | "failed"} [data.status]
+ * @param {*} [data.debug]
+ */
+export default function emit(data) {
+	if (LOGLEVEL !== 'debug') delete data.debug;
+
+	if (data.errors) {
+		if (!Array.isArray(data.errors.error)) {
+			return { success: false, errors: data.errors.error.flatten().fieldErrors };
+		} else {
+			return { success: false, errors: data.errors };
+		}
+	} else return { ...data, success: true };
+}
