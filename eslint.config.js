977294b6eccd025f12562125d907f0922648ad6d
Joshua Tahanlangit <contact@thisjt.me>
2024-10-11 00:16:40 +0800

    chore: create eslint prettier configs

---
+++ eslint.config.js
@@ -0,0 +1,85 @@
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+
+/**
+ * I have my own eslint and prettier config that I usually copy paste to all of
+ * my projects and I just tweak it based on the requirements of that project.
+ */
+
+export default [
+	prettier,
+	{
+		files: ['**/*.js'],
+		ignores: ['.env', '.env.*', '!.env.example', 'pnpm-lock.yaml'],
+		languageOptions: {
+			sourceType: 'module',
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			...{
+				'no-undef': ['error', { typeof: true }],
+				eqeqeq: 'error',
+				'no-fallthrough': 'off',
+				'no-case-declarations': 'off',
+				'keyword-spacing': 'error',
+				'max-len': [
+					'error',
+					{
+						code: 200,
+					},
+				],
+				'max-lines': [
+					'error',
+					{
+						max: 400,
+						skipComments: true,
+						skipBlankLines: true,
+					},
+				],
+				'max-lines-per-function': [
+					'error',
+					{
+						max: 150,
+						skipComments: true,
+						skipBlankLines: true,
+					},
+				],
+				quotes: ['error', 'single'],
+				semi: ['error', 'always'],
+				'comma-spacing': [
+					'error',
+					{
+						before: false,
+						after: true,
+					},
+				],
+				'no-template-curly-in-string': 'error',
+				'no-useless-return': 'error',
+				'no-console': 'warn',
+				'key-spacing': [
+					'error',
+					{
+						beforeColon: false,
+						afterColon: true,
+					},
+				],
+				'max-depth': ['error', 6],
+				'no-multiple-empty-lines': [
+					'error',
+					{
+						max: 3,
+					},
+				],
+				'no-trailing-spaces': 'error',
+				'one-var-declaration-per-line': 'error',
+				'no-var': 'error',
+				'no-inner-declarations': 'off',
+			},
+		},
+	},
+];
