61182b43af50108ae5b06940d702b52edddd9d4c
Joshua Tahanlangit <contact@thisjt.me>
2024-10-11 01:40:14 +0800

    refactor: include json and urlencoded middleware

--- src/lib/express.js
+++ src/lib/express.js
@@ -2,6 +2,9 @@ import express from 'express';
 
 export const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // middleware, i'm assuming we always send json to requestor
 app.use((request, response, next) => {
 	response.setHeader('content-type', 'application/json');
