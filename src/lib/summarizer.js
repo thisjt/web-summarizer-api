493166682ce9917575f92ff502c7bc245a0152ae
Joshua Tahanlangit <contact@thisjt.me>
2024-10-11 04:03:28 +0800

    refactor: add logger in summarizer

--- src/lib/summarizer.js
+++ src/lib/summarizer.js
@@ -1,12 +1,20 @@
 export class Summarizer {
 	/**
 	 * We will not be revalidating this, zod already does it for us
+	 * @param {number} id
 	 * @param {string} url
 	 */
-	constructor(url) {
+	constructor(id, url) {
 		this.url = url;
+		this.logger('Starting job id', id);
 
 		this.init();
+	}
+	url = '';
+	logs = '';
+	/**@param {(string | number)[]} text */
+	logger(...text) {
+		this.logs += `${text.join(' ')}\n`;
 	}
 
 	async init() {
