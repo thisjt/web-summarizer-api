488c9a2d28504466dc12329aaa5d0e4073a14cce
Joshua Tahanlangit <contact@thisjt.me>
2024-10-16 02:38:52 +0800

    refactor: fix ts types

--- src/lib/logging.ts
+++ src/lib/logging.ts
@@ -1,6 +1,6 @@
 const LOGLEVEL = process.env.LOGLEVEL;
 
 /**@param {*} data */
-export default function log(...data) {
+export default function log(...data: unknown[]) {
 	if (LOGLEVEL === 'debug') console.log(`${new Date().toLocaleTimeString()}: ${data.join(' ')}`);
 }
