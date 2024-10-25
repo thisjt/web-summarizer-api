8e005cc2dab49f015ae42f29645e542a58f09a00
Joshua Tahanlangit <contact@thisjt.me>
2024-10-26 02:42:53 +0800

    feat: integrate openapi to routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import getjob from './routes/getjob';
 import postjob from './routes/postjob';
+import { mountOpenApi } from './lib/openapi';
 
 const app = new OpenAPIHono();
 
 const routes = [getjob, postjob];
 
 routes.forEach((route) => app.route('/', route));
+
+mountOpenApi(app);
 
 export default app;
---
+++ src/lib/openapi.ts
@@ -0,0 +1,37 @@
+import { apiReference } from '@scalar/hono-api-reference';
+import packageJSON from './../../package.json';
+import type { OpenAPIHono } from '@hono/zod-openapi';
+
+export function mountOpenApi(app: OpenAPIHono) {
+	app.doc('/_openapischema', {
+		openapi: '3.1.0',
+		info: {
+			version: packageJSON.version,
+			title: `${packageJSON.properName} - Documentation`,
+			description: packageJSON.description,
+			contact: {
+				name: packageJSON.author,
+				url: packageJSON.authorUrl,
+				email: packageJSON.authorEmail,
+			},
+		},
+	});
+
+	app.get(
+		'/documentation',
+		apiReference({
+			theme: 'deepSpace',
+			layout: 'classic',
+			defaultHttpClient: {
+				targetKey: 'javascript',
+				clientKey: 'fetch',
+			},
+			hideDownloadButton: true,
+			withDefaultFonts: false,
+			defaultOpenAllTags: true,
+			spec: {
+				url: '/_openapischema',
+			},
+		})
+	);
+}
