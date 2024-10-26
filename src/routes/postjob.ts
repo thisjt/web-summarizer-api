5c10dc0c2655ac26073a260e5f2b0cface38970d
Joshua Tahanlangit <contact@thisjt.me>
2024-10-27 02:38:45 +0800

    feat: post job creation

--- src/routes/postjob.ts
+++ src/routes/postjob.ts
@@ -1,11 +1,13 @@
 import app from '../lib/app';
-import { z, createRoute } from '@hono/zod-openapi';
+import { createRoute } from '@hono/zod-openapi';
 import { jsonContent } from 'stoker/openapi/helpers';
 import * as StatusCodes from 'stoker/http-status-codes';
 import { createErrorSchema } from 'stoker/openapi/schemas';
-import { unauthorizedSchema } from '../lib/constants';
+import { internalServerErrorSchema, unauthorizedSchema } from '../lib/constants';
 import { JobCreate, JobDetails } from '../lib/models';
 import { TokenSchema } from '../lib/auth';
+import prisma from '../lib/prisma';
+import { INTERNAL_SERVER_ERROR } from 'stoker/http-status-phrases';
 
 const route = createRoute({
 	method: 'post',
@@ -18,19 +20,35 @@ const route = createRoute({
 		[StatusCodes.OK]: jsonContent(JobDetails, 'Job created from given url. Use the route /job/{id} to get job status'),
 		[StatusCodes.UNAUTHORIZED]: jsonContent(unauthorizedSchema, 'Unauthorized'),
 		[StatusCodes.BAD_REQUEST]: jsonContent(createErrorSchema(JobCreate), 'Error'),
+		[StatusCodes.INTERNAL_SERVER_ERROR]: jsonContent(internalServerErrorSchema, 'Internal Server Error'),
 	},
 });
 
-const getjob = app.openapi(route, async (c) => {
-	return c.json(
-		{
-			id: 3,
-			url: 'https:// - ',
-			status: 'asd',
-			timestamp: 123123,
-		},
-		StatusCodes.OK,
-	);
+const postjob = app.openapi(route, async (c) => {
+	const { url } = c.req.valid('json');
+
+	const timestamp = new Date().getTime();
+
+	let result;
+	try {
+		result = await prisma(c.env.DB).jobs.create({
+			data: {
+				url,
+				status: 'queue',
+				timestamp,
+			},
+			select: {
+				id: true,
+				url: true,
+				status: true,
+				timestamp: true,
+			},
+		});
+	} catch {
+		return c.json({ message: INTERNAL_SERVER_ERROR }, 500);
+	}
+
+	return c.json({ ...result, ...{ timestamp } }, StatusCodes.OK);
 });
 
-export default getjob;
+export default postjob;
