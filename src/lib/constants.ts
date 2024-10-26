f1dbbbac9a7f5e11415c1a4c4fe0d97cf947d0ab
Joshua Tahanlangit <contact@thisjt.me>
2024-10-26 12:54:45 +0800

    feat: add unauthorized schema

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,5 @@ import * as HttpStatusPhrases from 'stoker/http-status-phrases';
 import { createMessageObjectSchema } from 'stoker/openapi/schemas';
 
 export const notFoundSchema = createMessageObjectSchema(HttpStatusPhrases.NOT_FOUND);
+
+export const unauthorizedSchema = createMessageObjectSchema(HttpStatusPhrases.UNAUTHORIZED);
