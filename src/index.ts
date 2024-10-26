fb639498566897293b9c374a51be6632d864deb5
Joshua Tahanlangit <contact@thisjt.me>
2024-10-26 15:20:07 +0800

    refactor: add auth middleware for token

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,15 @@ import { OpenAPIHono } from '@hono/zod-openapi';
 import getjob from './routes/getjob';
 import postjob from './routes/postjob';
 import { mountOpenApi } from './lib/openapi';
+import auth from './lib/auth';
 
 const app = new OpenAPIHono();
+
+app.route('/', auth);
 
 const routes = [getjob, postjob];
 
 routes.forEach((route) => app.route('/', route));
-
-app.get('/', (c) => {
-	return c.redirect('/documentation', 302);
-});
 
 mountOpenApi(app);
 
