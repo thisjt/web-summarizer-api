777b422c62f85565fb2e717ee42f904340ec8307
Joshua Tahanlangit <contact@thisjt.me>
2024-10-27 03:50:37 +0800

    chore: bind browser

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,6 +1,7 @@
-import { D1Database } from '@cloudflare/workers-types';
+import { D1Database, Fetcher } from '@cloudflare/workers-types';
 
 export type Bindings = {
 	TOKEN: string;
 	DB: D1Database;
+	BROWSER: Fetcher;
 };
