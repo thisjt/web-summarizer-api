ce58df6e013d045c67415d9b4d64f40a40939aef
Joshua Tahanlangit <contact@thisjt.me>
2024-10-27 03:21:43 +0800

    feat: get all jobs

---
+++ src/routes/getalljobs.ts
@@ -0,0 +1,54 @@
+import app from '../lib/app';
+import { createRoute } from '@hono/zod-openapi';
+import { jsonContent } from 'stoker/openapi/helpers';
+import { internalServerErrorSchema, unauthorizedSchema } from '../lib/constants';
+import { TokenSchema } from '../lib/auth';
+import { MultipleJobDetails } from '../lib/models';
+import * as StatusCodes from 'stoker/http-status-codes';
+import * as StatusPhrases from 'stoker/http-status-phrases';
+import prisma from '../lib/prisma';
+
+const route = createRoute({
+	method: 'get',
+	path: '/jobs',
+	request: {
+		query: TokenSchema,
+	},
+	responses: {
+		[StatusCodes.OK]: jsonContent(MultipleJobDetails, 'Job Retrieved'),
+		[StatusCodes.UNAUTHORIZED]: jsonContent(unauthorizedSchema, 'Unauthorized'),
+		[StatusCodes.INTERNAL_SERVER_ERROR]: jsonContent(internalServerErrorSchema, 'Internal Server Error'),
+	},
+});
+
+const getalljobs = app.openapi(route, async (c) => {
+	let result;
+	try {
+		result = await prisma(c.env.DB).jobs.findMany({
+			select: {
+				id: true,
+				url: true,
+				status: true,
+				summary: true,
+				summary_logs: true,
+				summary_error_message: true,
+				timestamp: true,
+				started: true,
+				finished: true,
+			},
+		});
+	} catch (e) {
+		console.error(e);
+		return c.json({ message: StatusPhrases.INTERNAL_SERVER_ERROR }, StatusCodes.INTERNAL_SERVER_ERROR);
+	}
+
+	const zodded = MultipleJobDetails.safeParse(result);
+	if (!zodded.data) {
+		console.error('Failed Zod Values from database:', result, zodded.error);
+		return c.json({ message: StatusPhrases.INTERNAL_SERVER_ERROR }, StatusCodes.INTERNAL_SERVER_ERROR);
+	}
+
+	return c.json(zodded.data, StatusCodes.OK);
+});
+
+export default getalljobs;
