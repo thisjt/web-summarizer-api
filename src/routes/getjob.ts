2e771de0f5c5bf801593be98b173745881f1d96e
Joshua Tahanlangit <contact@thisjt.me>
2024-10-27 02:40:05 +0800

    refactor: include internal server erorr to responses

--- src/routes/getjob.ts
+++ src/routes/getjob.ts
@@ -3,7 +3,7 @@ import { z, createRoute } from '@hono/zod-openapi';
 import { jsonContent } from 'stoker/openapi/helpers';
 import * as StatusCodes from 'stoker/http-status-codes';
 import { createErrorSchema } from 'stoker/openapi/schemas';
-import { notFoundSchema, unauthorizedSchema } from '../lib/constants';
+import { internalServerErrorSchema, notFoundSchema, unauthorizedSchema } from '../lib/constants';
 import { TokenSchema } from '../lib/auth';
 import { JobDetails } from '../lib/models';
 
@@ -29,6 +29,7 @@ const route = createRoute({
 		[StatusCodes.NOT_FOUND]: jsonContent(notFoundSchema, 'Job ID not found'),
 		[StatusCodes.UNAUTHORIZED]: jsonContent(unauthorizedSchema, 'Unauthorized'),
 		[StatusCodes.BAD_REQUEST]: jsonContent(createErrorSchema(IdSchema), 'Error'),
+		[StatusCodes.INTERNAL_SERVER_ERROR]: jsonContent(internalServerErrorSchema, 'Internal Server Error'),
 	},
 });
 
