724287c51c9563a47e8d53d8615ce9b66463be63
Joshua Tahanlangit <contact@thisjt.me>
2024-10-27 02:38:35 +0800

    feat: create http status phrases internal server error

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,5 +2,5 @@ import * as HttpStatusPhrases from 'stoker/http-status-phrases';
 import { createMessageObjectSchema } from 'stoker/openapi/schemas';
 
 export const notFoundSchema = createMessageObjectSchema(HttpStatusPhrases.NOT_FOUND);
-
 export const unauthorizedSchema = createMessageObjectSchema(HttpStatusPhrases.UNAUTHORIZED);
+export const internalServerErrorSchema = createMessageObjectSchema(HttpStatusPhrases.INTERNAL_SERVER_ERROR);
