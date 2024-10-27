b04e3aaf93f108f4dbb4171159cf5cbdf0350501
Joshua Tahanlangit <contact@thisjt.me>
2024-10-28 02:48:57 +0800

    refactor: throw error on null data update

--- src/lib/summarizer/1.job.ts
+++ src/lib/summarizer/1.job.ts
@@ -13,7 +13,7 @@ const dbselect = {
 	finished: true,
 };
 export class JobReadUpdateDependency extends Summarizer {
-	readJob: JobRU['readJob'] = async ({ id }) => {
+	readJob: JobRU['readJob'] = async ({ id } = { id: this.id }) => {
 		if (!this.bindings) {
 			this.error('No bindings specified', this.bindings);
 			throw Error('No bindings specified');
@@ -36,12 +36,16 @@ export class JobReadUpdateDependency extends Summarizer {
 			this.error('Error readJob query', e);
 			return { success: false, error: { code: 2, message: 'Error readJob query' }, data: null };
 		}
-		return { success: true, error: null, data: result || {} };
+		return { success: true, error: null, data: result };
 	};
-	updateJob: JobRU['updateJob'] = async ({ id, data }) => {
+	updateJob: JobRU['updateJob'] = async ({ id, data } = { id: this.id, data: null }) => {
 		if (!this.bindings) {
 			this.error('No bindings specified', this.bindings);
 			throw Error('No bindings specified');
+		}
+		if (!data) {
+			this.error('No data provided', data);
+			throw Error('No data provided');
 		}
 		if (!id) id = this.id;
 
@@ -49,12 +53,13 @@ export class JobReadUpdateDependency extends Summarizer {
 		try {
 			result = await prisma(this.bindings.DB).jobs.update({
 				data,
+				select: dbselect,
 				where: { id },
 			});
 		} catch (e) {
 			this.error('Error updateJob query', e);
 			return { success: false, error: { code: 2, message: 'Error updateJob query' }, data: null };
 		}
-		return { success: true, error: null, data: result || {} };
+		return { success: true, error: null, data: result || null };
 	};
 }
