e15e1b64cddfcc340f4a916db67256d0bd72abf1
Joshua Tahanlangit <contact@thisjt.me>
2024-10-27 13:53:48 +0800

    refactor: transfer execute logic from express to hono

--- src/routes/executejob.ts
+++ src/routes/executejob.ts
@@ -9,6 +9,9 @@ import * as StatusCodes from 'stoker/http-status-codes';
 import * as StatusPhrases from 'stoker/http-status-phrases';
 import prisma from '../lib/prisma';
 import { IdSchema } from '../lib/models';
+import puppeteer from '@cloudflare/puppeteer';
+
+const SHORT_PARAGRAPH = 100; //characters
 
 const route = createRoute({
 	method: 'get',
@@ -30,24 +33,31 @@ const route = createRoute({
 const executejob = app.openapi(route, async (c) => {
 	const { id } = c.req.valid('param');
 
+	const dbselect = {
+		id: true,
+		url: true,
+		status: true,
+		summary: true,
+		summary_logs: true,
+		summary_error_message: true,
+		timestamp: true,
+		started: true,
+		finished: true,
+	};
+
 	let result;
 	try {
-		result = await prisma(c.env.DB).jobs.findUnique({
-			select: {
-				id: true,
-				url: true,
-				status: true,
-				summary: true,
-				summary_logs: true,
-				summary_error_message: true,
-				timestamp: true,
-				started: true,
-				finished: true,
-			},
-			where: {
-				id,
-			},
-		});
+		if (id) {
+			result = await prisma(c.env.DB).jobs.findFirst({
+				select: dbselect,
+				where: { id },
+			});
+		} else {
+			result = await prisma(c.env.DB).jobs.findFirst({
+				select: dbselect,
+				where: { status: 'queue' },
+			});
+		}
 	} catch (e) {
 		console.error(e);
 		return c.json({ message: StatusPhrases.INTERNAL_SERVER_ERROR }, StatusCodes.INTERNAL_SERVER_ERROR);
@@ -55,13 +65,62 @@ const executejob = app.openapi(route, async (c) => {
 
 	if (!result) return c.json({ message: StatusPhrases.NOT_FOUND }, StatusCodes.NOT_FOUND);
 
-	const zodded = JobDetails.safeParse(result);
-	if (!zodded.data) {
-		console.error('Failed Zod Values from database:', result, zodded.error);
+	const ParsedJobDetails = JobDetails.safeParse(result);
+	if (!ParsedJobDetails.data) {
+		console.error('Failed Zod Values from database:', result, ParsedJobDetails.error);
 		return c.json({ message: StatusPhrases.INTERNAL_SERVER_ERROR }, StatusCodes.INTERNAL_SERVER_ERROR);
 	}
 
-	return c.json(zodded.data, StatusCodes.OK);
+	// Step 1. Set Status
+	try {
+		// await prisma(c.env.DB).jobs.update({
+		// 	data: {
+		// 		status: 'processing',
+		// 		started: new Date().getTime(),
+		// 	},
+		// 	where: { id: result.id },
+		// });
+	} catch (e) {
+		console.error(e);
+		return c.json({ message: StatusPhrases.INTERNAL_SERVER_ERROR }, StatusCodes.INTERNAL_SERVER_ERROR);
+	}
+
+	// Step 2. Load URL to Puppeteer
+	let htmlString = '';
+	try {
+		const browser = await puppeteer.launch(c.env.BROWSER);
+		const page = await browser.newPage();
+		await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15');
+		await page.goto(result.url, { waitUntil: 'load', timeout: 15000 });
+		const bodyHandle = await page.$('body');
+		const html = await page.evaluate((body) => {
+			let pData = '';
+			/**
+			 * Grabbing text inside the <p> tags are better than trying to filter out
+			 * unwanted elements. This is a design choice, but it can be better. We do lose
+			 * a bit of potentially useful data for summarization though.
+			 */
+			[...(body?.querySelectorAll('p') || [])].forEach((p) => {
+				pData += p.textContent + ' ';
+			});
+			return pData;
+		}, bodyHandle);
+		htmlString = html.replace(/<\/?[^>]+(>|$)/g, '');
+		await browser.close();
+	} catch (e) {
+		console.error(e);
+		return c.json({ message: StatusPhrases.INTERNAL_SERVER_ERROR }, StatusCodes.INTERNAL_SERVER_ERROR);
+	}
+
+	// Step 3. Clean up HTML String
+	const htmlStringExploded = htmlString.split('\n').map((line) => line.replace(/\s+/g, ' ').trim());
+	htmlStringExploded.forEach((v, i) => (htmlStringExploded[i] = htmlStringExploded[i].trim()));
+	const htmlFilterShortStrings = htmlStringExploded.filter((str) => str.length > SHORT_PARAGRAPH);
+	htmlString = htmlFilterShortStrings.join(' ');
+
+	console.log(htmlString);
+
+	return c.json(ParsedJobDetails.data, StatusCodes.OK);
 });
 
 export default executejob;
