2497f41fdc074f531893ed497c43b8b73625e408
Joshua Tahanlangit <contact@thisjt.me>
2024-10-27 13:52:35 +0800

    refactor: add browser worker type

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,7 +1,8 @@
-import { D1Database, Fetcher } from '@cloudflare/workers-types';
+import { BrowserWorker } from '@cloudflare/puppeteer';
+import { D1Database } from '@cloudflare/workers-types';
 
 export type Bindings = {
 	TOKEN: string;
 	DB: D1Database;
-	BROWSER: Fetcher;
+	BROWSER: BrowserWorker;
 };
